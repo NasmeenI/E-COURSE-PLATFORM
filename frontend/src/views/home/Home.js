c878708cfb66bf853c5e712611204b4378668870
athensclub <43963760+athensclub@users.noreply.github.com>
2023-01-09 00:05:06 +0700

    add footer component

--- frontend/src/views/home/Home.js
+++ frontend/src/views/home/Home.js
@@ -1,3 +1,4 @@
+import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import logo from "../../logo.svg";
 
@@ -5,6 +6,8 @@ export default function Home() {
   return (
     <div>
       <Header />
+      
+      <Footer />
     </div>
   );
 }
