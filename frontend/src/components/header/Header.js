ab1b66c002ee0339adeb26ff8b172fa4a0bb8f61
athensclub <43963760+athensclub@users.noreply.github.com>
2023-01-08 22:00:13 +0700

    make header fixed

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="flex flex-row justify-evenly items-center w-full">
+    <header className="fixed top-0 flex flex-row justify-evenly items-center w-full">
       <span className="text-xl font-primary">E-Learning</span>
       <span className="text-xl font-secondary">Feature 1</span>
       <span className="text-xl font-secondary">Feature 2</span>
