8e1affb2d5704291260d1f87354396eb6e9f7248
Zenith <sqoulo11@gmail.com>
2023-01-09 00:51:58 +0700

    home page body

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header className="fixed flex flex-row justify-evenly items-center w-full h-[60px] border-b-[1px] border-[#E0E0E0]">
-      <div className="flex flex-row items-center h-full">
+    <header className="fixed top-0 flex flex-row justify-evenly items-center w-full h-[60px] border-b-[1px] border-[#E0E0E0] bg-white">
+      <div className="flex flex-row items-center h-full ">
         <span className="text-xl font-primary">E-Learning</span>
         <div className="w-[3px] h-[30%] bg-[#E0E0E0] ml-[30px]"></div>
         <span className="text-xl font-secondary ml-[30px]">Feature 1</span>
@@ -10,7 +10,7 @@ export default function Header() {
       </div>
       <div className="flex flex-row items-center h-full">
         <span className="text-xl font-secondary">Passa</span>
-        <span className="text-xl font-secondary ml-[30px] font-extrabold">Sign Out</span>
+        <span className="text-xl font-secondary ml-[30px] font-extrabold ">Sign Out</span>
       </div>
       
     </header>
