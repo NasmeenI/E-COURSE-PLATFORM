663749391733e53762f2981da54592475c4befb4
athensclub <43963760+athensclub@users.noreply.github.com>
2023-01-08 21:26:04 +0700

    create header component

---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="flex flex-row justify-between items-center w-full">
+      <span className="text-xl font-extrabold">E-Learning</span>
+    </header>
+  );
+}
--- frontend/src/views/home/Home.js
+++ frontend/src/views/home/Home.js
@@ -1,20 +1,10 @@
+import Header from "../../components/header/Header";
 import logo from "../../logo.svg";
 
 export default function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Passakorn with 3,500,000,000 people.</p>
-        <a
-          className="App-link text-3xl"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          We love Passakorn.
-        </a>
-      </header>
+    <div>
+      <Header />
     </div>
   );
 }
