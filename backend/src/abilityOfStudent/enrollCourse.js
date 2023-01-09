596db0e1c1249be89ede49116073d46f9fcd45aa
NasmeenI <nasmeen2503@gmail.com>
2023-01-09 15:53:53 +0700

    ability of student
    add complete enroll course

--- backend/src/abilityOfStudent/enrollCourse.js
+++ backend/src/abilityOfStudent/enrollCourse.js
@@ -11,11 +11,11 @@ export const enrollCourse = async (req ,res) => {
     try{
         // read old courses from database 
         const studentRef = db.collection('student').doc('PmzuI3FMlZ7yxjZdOoYt');
-        const doc = await studentRef.get()
-        if(!doc.exists){
+        const docStudent = await studentRef.get()
+        if(!docStudent.exists){
             return res.sendStatus(400);
         }
-        const oldCourses = doc.data().courses;
+        const oldCourses = docStudent.data().courses;
 
         // add new course in student's data
         const { newCourse } = req.body;
@@ -24,9 +24,23 @@ export const enrollCourse = async (req ,res) => {
         const res2 = await studentRef.set({
             ["courses"] : newCourses
         })
-        res.status(200).send(newCourses);
 
-        // add student's data in course
+        // read old student from database 
+        const courseRef = db.collection('courses').doc('SJri6hRBQDeHKoClWVGE');
+        const docCourse = await courseRef.get()
+        if(!docCourse.exists){
+            return docCourse.sendStatus(400);
+        }
+        const oldStudents = docCourse.data().students;
+
+        // add new student in course's data
+        const { newStudent } = req.body;
+        const newStudents = oldStudents;
+        newStudents.push(newStudent);
+        const res3 = await courseRef.set({
+            ["students"] : newStudents
+        })
+        res.status(200).send("enroll new course complete");
     }
     catch(error) {
         res.send(error);
