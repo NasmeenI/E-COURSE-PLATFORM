4d4711c06194e26e1c28260251a29c73910c1abc
NasmeenI <nasmeen2503@gmail.com>
2023-01-09 14:17:32 +0700

    abilityOfInstructor_createCourse

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,34 +1,31 @@
+// app
 import express from 'express';
-
-import { db } from './firebase.js';
-import bp from 'body-parser';
-import { FieldValue } from 'firebase-admin/firestore';
 const app = express();
+
+// body-parser
+import bp from 'body-parser';
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
-
-
-import { create } from './routes/create.js';
-import { read } from './routes/read.js';
-import { update } from './routes/update.js';
-import { deleteDoc } from './routes/deleteDoc.js';
-import { deleteField } from './routes/deleteField.js';
-
-// const app = express();
 
 app.get('/' ,function(req ,res){
     res.send('Hello');
 })
 
+// routes
+import { create } from './routes/create.js';
+import { read } from './routes/read.js';
+import { update } from './routes/update.js';
+import { deleteDoc } from './routes/deleteDoc.js';
+import { deleteField } from './routes/deleteField.js';
 app.get('/friends' ,read)
-
 app.post('/addfriends' ,create)
-
 app.patch('/change' ,update)
-
 app.delete('/field' ,deleteField);
-
 app.delete('/doc' ,deleteDoc);
+
+// ability of instructors
+import{ createCourse } from './abilityOfInstructor/createCourse.js';
+app.post('/createCourse' ,createCourse);
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
