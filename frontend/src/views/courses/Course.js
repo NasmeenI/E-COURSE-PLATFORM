1095226c0f80bb0e4f709ee6755d6e65d6bc4de4
Zenith <sqoulo11@gmail.com>
2023-01-09 16:15:15 +0700

    edit courses body

---
+++ frontend/src/views/courses/Course.js
@@ -0,0 +1,6 @@
+import pic5 from "../courses/assets/pic5.png";
+export default function Course(prop) {
+    return <div className="my-[20px] bg-white flex flex-row rounded-lg">
+        <img src={pic5} alt="pic5" className="w-[30%] rounded-l-lg"></img>
+    </div>
+}
--- frontend/src/views/courses/Courses.js
+++ frontend/src/views/courses/Courses.js
@@ -1,11 +1,31 @@
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
+import Course from "./Course";
 
 export default function Courses() {
   return (
     <div>
       <Header />
+      {/* body */}
+      <div className="bg-[#F6F5F4] mt-[60px] w-full flex flex-col justify-evenly items-center">
+        {/* Courses box */}
+        <div className="bg-white m-[30px] w-[60%] py-[15px] rounded-lg">
+          <span className="text-right font-primary text-[26px] ml-[15px]">Courses</span>
+        </div>
 
+        {/* Tag */}
+        <div className="flex flex-row justify-evenly items-center">
+          <span className="m-[10px] text-[20px] font-primary">Calculus</span>
+          <span className="m-[10px] text-[20px] font-primary">Physics</span>
+          <span className="m-[10px] text-[20px] font-primary">Chemistry</span>
+          <span className="m-[10px] text-[20px] font-primary">Biology</span>
+        </div>
+
+        {/* Course */}
+        <div className="flex flex-col justify-evenly items-center">
+          <Course />
+        </div>
+      </div>
       <Footer />
     </div>
   );
