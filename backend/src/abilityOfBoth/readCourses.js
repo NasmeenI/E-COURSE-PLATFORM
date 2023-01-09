cfcd52dda8607d1618f4d3bd74a866bef1a7cc18
NasmeenI <nasmeen2503@gmail.com>
2023-01-09 21:45:41 +0700

    specify page of response

--- backend/src/abilityOfBoth/readCourses.js
+++ backend/src/abilityOfBoth/readCourses.js
@@ -8,16 +8,25 @@ app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
 
 export const readCourses = async (req ,res) => { 
-    try{
-        const { type ,name } = req.body;
-        const collectionRef = db.collection(type).doc(name);
-        const doc = await collectionRef.get()
-        if(!doc.exists){
-            return res.sendStatus(400);
-        }
-        res.status(200).send(doc.data().courses);
+    const { type ,name ,page} = req.body;
+    const collectionRef = db.collection(type).doc(name);
+    const snapshot = await collectionRef.get()
+    if(!snapshot.exists){
+        return res.sendStatus(400);
     }
-    catch(error) {
-        res.send(error);
+    let courses = []
+
+    for(let i=5*(Number(page)-1);i<=Math.min((Number(page)*5)-1 ,snapshot.data().courses.length-1);i++){
+        const coursesRef = db.collection('courses');
+        const snapshot1 = await coursesRef.where('title', '==', snapshot.data().courses[i]).get();
+        if (snapshot1.empty) {
+            console.log('No matching documents.');
+        }  
+    
+        let output = snapshot1.docs[0].data()
+        if(type != 'instructor') output.students = null
+        
+        courses.push(output)
     }
+    res.send(courses);
 }
