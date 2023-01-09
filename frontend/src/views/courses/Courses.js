dcc8cd02ee1704286c349de694dc6937d3c65de3
Zenith <sqoulo11@gmail.com>
2023-01-09 17:08:01 +0700

    add course component

--- frontend/src/views/courses/Courses.js
+++ frontend/src/views/courses/Courses.js
@@ -9,16 +9,16 @@ export default function Courses() {
       {/* body */}
       <div className="bg-[#F6F5F4] mt-[60px] w-full flex flex-col justify-evenly items-center">
         {/* Courses box */}
-        <div className="bg-white m-[30px] w-[60%] py-[15px] rounded-lg">
+        <div className="bg-white m-[30px] w-[70%] py-[15px] rounded-lg">
           <span className="text-right font-primary text-[26px] ml-[15px]">Courses</span>
         </div>
 
         {/* Tag */}
         <div className="flex flex-row justify-evenly items-center">
-          <span className="m-[10px] text-[20px] font-primary">Calculus</span>
-          <span className="m-[10px] text-[20px] font-primary">Physics</span>
-          <span className="m-[10px] text-[20px] font-primary">Chemistry</span>
-          <span className="m-[10px] text-[20px] font-primary">Biology</span>
+          <span className="m-[10px] text-[20px] font-primary">Tag1</span>
+          <span className="m-[10px] text-[20px] font-primary">Tag2</span>
+          <span className="m-[10px] text-[20px] font-primary">Tag3</span>
+          <span className="m-[10px] text-[20px] font-primary">Tag4</span>
         </div>
 
         {/* Course */}
