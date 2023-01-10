ef615db876cd12ad4ed489402b6beece75eae730
NasmeenI <nasmeen2503@gmail.com>
2023-01-10 23:12:44 +0700

    Nasmeen's API

--- frontend/src/api/NasmeenAPI.js
+++ frontend/src/api/NasmeenAPI.js
@@ -12,8 +12,47 @@ async function enrollCourse(userID, courseID) {
   return result;
 }
 
+async function createCourse(_id, title ,class1 ,instructorName ,description) {
+  const result = await axios.patch(endpoint("/createCourse"), {
+    _id : _id,
+    title : title,
+    class1 : class1,
+    instructorName : instructorName,
+    description : description
+  });
+  return result;
+}
+
+async function readAllCourses(classOfCourse, page) {
+  const result = await axios.patch(endpoint("/readAllCourses"), {
+    classOfCourse : classOfCourse,
+    page : page
+  });
+  return result;
+}
+
+async function readCourses(userID, page) {
+  const result = await axios.patch(endpoint("/readCourses"), {
+    userID : userID,
+    page : page
+  });
+  return result;
+}
+
+async function readEachCourses(courseID) {
+  const result = await axios.patch(endpoint("/readEachCourses"), {
+    courseID : courseID,
+  });
+  return result;
+}
+
 const NasmeenAPI = {
   enrollCourse,
+  createCourse,
+  readAllCourses,
+  readCourses,
+  readEachCourses
 };
 
 export default NasmeenAPI;
+
