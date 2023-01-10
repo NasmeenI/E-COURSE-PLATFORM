95b6da95943befca98a96670c8e00704412edc86
athensclub <43963760+athensclub@users.noreply.github.com>
2023-01-10 13:36:01 +0700

    refactor: page button conditional rendering

--- frontend/src/views/courses/PageButton.js
+++ frontend/src/views/courses/PageButton.js
@@ -1,15 +1,11 @@
 export default function PageButton({ current, page }) {
-  if (current) {
-    return (
-      <span className="w-12 h-12 bg-[#2B788B] border-[#2B788B] border-2 text-white font-secondary font-bold text-[16px] rounded-full flex items-center justify-evenly mx-[10px]">
-        {page}
-      </span>
-    );
-  } else {
-    return (
-      <span className="w-12 h-12 bg-white border-[#2B788B] border-2 text-[#2B788B] font-secondary font-bold text-[16px] rounded-full flex items-center justify-evenly mx-[10px]">
-        {page}
-      </span>
-    );
-  }
+  return (
+    <span
+      className={`w-12 h-12  border-[#2B788B] border-2 font-secondary font-bold text-[16px] rounded-full flex items-center justify-center mx-[10px] ${
+        current ? "bg-[#2B788B] text-white" : "bg-white text-[#2B788B]"
+      }`}
+    >
+      {page}
+    </span>
+  );
 }
