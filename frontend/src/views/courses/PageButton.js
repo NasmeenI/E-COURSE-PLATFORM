558406a8720369db73012c4b3ce6bbf9a4cb99c1
athensclub <43963760+athensclub@users.noreply.github.com>
2023-01-10 13:12:32 +0700

    edit: explicit props name

--- frontend/src/views/courses/PageButton.js
+++ frontend/src/views/courses/PageButton.js
@@ -1,7 +1,15 @@
-export default function PageButton(prop) {
-    if (prop.current) {
-        return <span className="w-12 h-12 bg-[#2B788B] border-[#2B788B] text-white font-secondary font-bold text-[16px] rounded-full flex items-center justify-evenly mx-[10px]">{prop.page}</span>
-    } else {
-        return <span className="w-12 h-12 bg-white border-[#2B788B] text-[#2B788B] font-secondary font-bold text-[16px] rounded-full flex items-center justify-evenly mx-[10px]">{prop.page}</span>
-    };
-};
+export default function PageButton({ current, page }) {
+  if (current) {
+    return (
+      <span className="w-12 h-12 bg-[#2B788B] border-[#2B788B] text-white font-secondary font-bold text-[16px] rounded-full flex items-center justify-evenly mx-[10px]">
+        {page}
+      </span>
+    );
+  } else {
+    return (
+      <span className="w-12 h-12 bg-white border-[#2B788B] text-[#2B788B] font-secondary font-bold text-[16px] rounded-full flex items-center justify-evenly mx-[10px]">
+        {page}
+      </span>
+    );
+  }
+}
