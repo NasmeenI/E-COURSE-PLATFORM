f167fc44b2c9852bbb51b280f39a2359c33f91fb
athensclub <43963760+athensclub@users.noreply.github.com>
2023-01-11 14:57:08 +0700

    add: loading spinner in register page

--- frontend/src/views/register/Register.js
+++ frontend/src/views/register/Register.js
@@ -3,8 +3,8 @@ import Header from "../../components/header/Header";
 import { useContext, useState } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import UserAPI from "../../api/UserAPI";
-import { toast } from "react-hot-toast";
 import NasmeenAPI from "../../api/NasmeenAPI";
+import { TailSpin } from "react-loader-spinner";
 
 export default function Register() {
   const [firstName, setFirstName] = useState("");
@@ -16,6 +16,8 @@ export default function Register() {
   const [role, setRole] = useState("student");
 
   const [errorMessage, setErrorMessage] = useState(null);
+
+  const [registering, setRegistering] = useState(false);
 
   const {
     data: { user },
@@ -53,6 +55,7 @@ export default function Register() {
   async function handleSubmit(event) {
     event.preventDefault();
     setErrorMessage(null);
+    setRegistering(true);
     const result = await UserAPI.register(email, password);
     if (result.error) {
       setErrorMessage(result.error);
@@ -66,6 +69,7 @@ export default function Register() {
       );
       console.log(regisResult);
     }
+    setRegistering(false);
   }
 
   return (
@@ -73,130 +77,149 @@ export default function Register() {
       <Header />
       <div className="mt-[60px] flex flex-col items-center bg-[#F6F5F4] w-full justify-evenly">
         <div className="w-[40%] bg-white my-[50px] flex flex-col p-[50px] rounded-md items-center">
-          {errorMessage ? (
-            <div className="bg-red-200 py-3 rounded-xl w-full flex flex-col items-center justify-center mb-4">
-              <span className="text-red-600 font-bold font-secondary">
-                Error
-              </span>
-              <span className="text-red-600 font-secondary">
-                {errorMessage}
-              </span>
-            </div>
+          {registering ? (
+            <TailSpin
+              height="80"
+              width="80"
+              color="#2B788B"
+              ariaLabel="tail-spin-loading"
+              radius="1"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+            />
           ) : (
-            <></>
-          )}
+            <>
+              {errorMessage ? (
+                <div className="bg-red-200 py-3 rounded-xl w-full flex flex-col items-center justify-center mb-4">
+                  <span className="text-red-600 font-bold font-secondary">
+                    Error
+                  </span>
+                  <span className="text-red-600 font-secondary">
+                    {errorMessage}
+                  </span>
+                </div>
+              ) : (
+                <></>
+              )}
 
-          <span className="font-primary text-[32px] mb-[30px]">Sign In</span>
-
-          {/* Register with google button */}
-          <button
-            onClick={""}
-            className="w-[70%] bg-blue-600 rounded-lg py-[5px]"
-          >
-            <span className="text-center text-white font-secondary text-[16px] font-extrabold">
-              Sign in with Google
-            </span>
-          </button>
-
-          <div className="w-[70%] h-[3px] bg-[#E0E0E0] my-[20px]"></div>
-
-          {/* input box */}
-          <div className="w-[70%] items-start flex flex-col">
-            {/* First Name textbox */}
-            <span className="font-secondary text-[16px]">First Name :</span>
-            <input
-              type="text"
-              name="firstname"
-              value={firstName}
-              onChange={handleFirstNameChange}
-              className="w-full border-[1px] border-[#2B788B] rounded-lg mb-[10px] mt-[10px] px-[10px] pb-[3px] text-[16px] font-secondary"
-            />
-
-            {/* Username textbox */}
-            <span className="font-secondary text-[16px]">Last Name :</span>
-            <input
-              type="text"
-              name="lastname"
-              value={lastName}
-              onChange={handleLastNameChange}
-              className="w-full border-[1px] border-[#2B788B] rounded-lg mb-[10px] mt-[10px] px-[10px] pb-[3px] text-[16px] font-secondary"
-            />
-
-            <span className="font-secondary text-[16px]">Email :</span>
-            {/* Email textbox */}
-            <input
-              type="text"
-              name="email"
-              value={email}
-              onChange={handleEmailChange}
-              className="w-full border-[1px] border-[#2B788B] rounded-lg mb-[10px] mt-[10px] px-[10px] pb-[3px] text-[16px] font-secondary"
-            />
-
-            <span className="font-secondary text-[16px]">Password :</span>
-            {/* Password textbox */}
-            <input
-              type="password"
-              name="password"
-              value={password}
-              onChange={handlePasswordChange}
-              className="w-full border-[1px] border-[#2B788B] rounded-lg mb-[10px] mt-[10px] px-[10px] pb-[3px] text-[16px] font-secondary"
-            />
-            <span className="font-secondary text-[16px]">
-              Confirm Password :
-            </span>
-            {/* Confirm Password textbox */}
-            <input
-              type="password"
-              name="confirmPassword"
-              value={confirmPassword}
-              onChange={handleConfirmPasswordChange}
-              className="w-full border-[1px] border-[#2B788B] rounded-lg mb-[10px] mt-[10px] px-[10px] pb-[3px] text-[16px] font-secondary"
-            />
-          </div>
-
-          {/* Role Selection */}
-          <div className="flex flex-row w-[70%] justify-around rounded-xl overflow-hidden mt-3 border border-black/[0.1]">
-            <button
-              className={`font-secondary flex flex-1 items-center justify-center text-[14px] py-3 ${
-                role === "student"
-                  ? "bg-blue-600 text-white"
-                  : "bg-white text-black"
-              }`}
-              onClick={setStudent}
-            >
-              I'm a Student
-            </button>
-
-            <button
-              className={`font-secondary flex flex-1 items-center justify-center text-[15px] py-3 ${
-                role === "instructor"
-                  ? "bg-blue-600 text-white"
-                  : "bg-white text-black"
-              }`}
-              onClick={setInstructor}
-            >
-              I'm an Instructor
-            </button>
-          </div>
-
-          {/* Register button */}
-          <button
-            onClick={handleSubmit}
-            className="w-[70%] bg-[#2B788B] rounded-lg py-[5px] mt-[20px]"
-          >
-            <span className="text-white text-[16px] font-extrabold text-center font-secondary">
-              Sign In
-            </span>
-          </button>
-          <div className="flex flex-row mt-[20px] items-baseline">
-            <span className="font-secondary">Already have an account ?</span>
-            {/* Login text */}
-            <button onClick={handleSubmit}>
-              <span className="text-[#2B788B] text-[16px] underline ml-[15px] font-extrabold text-center font-secondary">
-                Login
+              <span className="font-primary text-[32px] mb-[30px]">
+                Sign In
               </span>
-            </button>
-          </div>
+
+              {/* Register with google button */}
+              <button
+                onClick={""}
+                className="w-[70%] bg-blue-600 rounded-lg py-[5px]"
+              >
+                <span className="text-center text-white font-secondary text-[16px] font-extrabold">
+                  Sign in with Google
+                </span>
+              </button>
+
+              <div className="w-[70%] h-[3px] bg-[#E0E0E0] my-[20px]"></div>
+
+              {/* input box */}
+              <div className="w-[70%] items-start flex flex-col">
+                {/* First Name textbox */}
+                <span className="font-secondary text-[16px]">First Name :</span>
+                <input
+                  type="text"
+                  name="firstname"
+                  value={firstName}
+                  onChange={handleFirstNameChange}
+                  className="w-full border-[1px] border-[#2B788B] rounded-lg mb-[10px] mt-[10px] px-[10px] pb-[3px] text-[16px] font-secondary"
+                />
+
+                {/* Username textbox */}
+                <span className="font-secondary text-[16px]">Last Name :</span>
+                <input
+                  type="text"
+                  name="lastname"
+                  value={lastName}
+                  onChange={handleLastNameChange}
+                  className="w-full border-[1px] border-[#2B788B] rounded-lg mb-[10px] mt-[10px] px-[10px] pb-[3px] text-[16px] font-secondary"
+                />
+
+                <span className="font-secondary text-[16px]">Email :</span>
+                {/* Email textbox */}
+                <input
+                  type="text"
+                  name="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  className="w-full border-[1px] border-[#2B788B] rounded-lg mb-[10px] mt-[10px] px-[10px] pb-[3px] text-[16px] font-secondary"
+                />
+
+                <span className="font-secondary text-[16px]">Password :</span>
+                {/* Password textbox */}
+                <input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  className="w-full border-[1px] border-[#2B788B] rounded-lg mb-[10px] mt-[10px] px-[10px] pb-[3px] text-[16px] font-secondary"
+                />
+                <span className="font-secondary text-[16px]">
+                  Confirm Password :
+                </span>
+                {/* Confirm Password textbox */}
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  value={confirmPassword}
+                  onChange={handleConfirmPasswordChange}
+                  className="w-full border-[1px] border-[#2B788B] rounded-lg mb-[10px] mt-[10px] px-[10px] pb-[3px] text-[16px] font-secondary"
+                />
+              </div>
+
+              {/* Role Selection */}
+              <div className="flex flex-row w-[70%] justify-around rounded-xl overflow-hidden mt-3 border border-black/[0.1]">
+                <button
+                  className={`font-secondary flex flex-1 items-center justify-center text-[14px] py-3 ${
+                    role === "student"
+                      ? "bg-blue-600 text-white"
+                      : "bg-white text-black"
+                  }`}
+                  onClick={setStudent}
+                >
+                  I'm a Student
+                </button>
+
+                <button
+                  className={`font-secondary flex flex-1 items-center justify-center text-[15px] py-3 ${
+                    role === "instructor"
+                      ? "bg-blue-600 text-white"
+                      : "bg-white text-black"
+                  }`}
+                  onClick={setInstructor}
+                >
+                  I'm an Instructor
+                </button>
+              </div>
+
+              {/* Register button */}
+              <button
+                onClick={handleSubmit}
+                className="w-[70%] bg-[#2B788B] rounded-lg py-[5px] mt-[20px]"
+              >
+                <span className="text-white text-[16px] font-extrabold text-center font-secondary">
+                  Sign In
+                </span>
+              </button>
+              <div className="flex flex-row mt-[20px] items-baseline">
+                <span className="font-secondary">
+                  Already have an account ?
+                </span>
+                {/* Login text */}
+                <button onClick={handleSubmit}>
+                  <span className="text-[#2B788B] text-[16px] underline ml-[15px] font-extrabold text-center font-secondary">
+                    Login
+                  </span>
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </div>
 
