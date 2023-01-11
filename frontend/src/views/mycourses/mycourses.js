afaacbedfd07851ba8a061585ccc2893ca2de062
Zenith <sqoulo11@gmail.com>
2023-01-11 19:13:07 +0700

    edit: MyCourse page

--- frontend/src/views/mycourses/mycourses.js
+++ frontend/src/views/mycourses/mycourses.js
@@ -1,3 +1,15 @@
-export default function Mycourses() {
-    return <div></div>
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import MyCourseCard from "./MyCoursesCard";
+
+export default function MyCourses() {
+    return <div>
+        <Header />
+        <div className="mt-[60px] flex flex-col bg-[#F6F5F4] w-full">
+            <ol>
+                <MyCourseCard />
+            </ol>
+        </div>
+        <Footer />
+    </div>
 }
