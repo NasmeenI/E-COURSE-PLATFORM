d0e97460fe1e713939b57629d11529c9d6c8a7e9
athensclub <43963760+athensclub@users.noreply.github.com>
2023-01-11 20:42:41 +0700

    Create MyCourses.js

---
+++ frontend/src/views/mycourses/MyCourses.js
@@ -0,0 +1,28 @@
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import MyCourseCard from "./MyCoursesCard";
+
+export default function MyCourses() {
+  return (
+    <div>
+      <Header />
+      <div className="mt-[60px] flex flex-col bg-[#F6F5F4] w-full">
+        <div className="flex flex-col items-start mb-[40px] mt-[40px] mx-[40px] bg-white">
+          <span className="font-primary mt-[50px] ml-[50px] text-[32px]">
+            My Courses
+          </span>
+          <ol className=" flex flex-wrap justify-evenly">
+            <MyCourseCard />
+            <MyCourseCard />
+            <MyCourseCard />
+            <MyCourseCard />
+            <MyCourseCard />
+            <MyCourseCard />
+            <MyCourseCard />
+          </ol>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
