9f386228de82e9e7e91aade8476ae413629e6c4b
athensclub <43963760+athensclub@users.noreply.github.com>
2023-01-11 14:44:38 +0700

    edit: use first name and last name in register page

--- frontend/src/views/register/Register.js
+++ frontend/src/views/register/Register.js
@@ -4,15 +4,18 @@ import { useContext, useState } from "react";
 import { UserContext } from "../../contexts/UserContext";
 import UserAPI from "../../api/UserAPI";
 import { toast } from "react-hot-toast";
+import NasmeenAPI from "../../api/NasmeenAPI";
 
 export default function Register() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const [checkStudent, setCheckStudent] = useState("");
-  const [checkInstructor, setCheckInstructor] = useState("");
+  const [role, setRole] = useState("student");
+
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const {
     data: { user },
@@ -22,23 +25,46 @@ export default function Register() {
   function handleEmailChange(event) {
     setEmail(event.target.value);
   }
-  function handleUsernameChange(event) {
-    setUsername(event.target.value);
+
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
   }
+
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  }
+
   function handlePasswordChange(event) {
     setPassword(event.target.value);
   }
+
   function handleConfirmPasswordChange(event) {
     setConfirmPassword(event.target.value);
   }
 
+  function setStudent() {
+    setRole("student");
+  }
+
+  function setInstructor() {
+    setRole("instructor");
+  }
+
   async function handleSubmit(event) {
     event.preventDefault();
+    setErrorMessage(null);
     const result = await UserAPI.register(email, password);
     if (result.error) {
-      toast("Error " + result.error);
+      setErrorMessage(result.error);
     } else {
-      console.log(result.user.uid);
+      console.log(result);
+      const regisResult = await NasmeenAPI.createAccount(
+        result.user.uid,
+        firstName,
+        lastName,
+        role
+      );
+      console.log(regisResult);
     }
   }
 
@@ -47,6 +73,14 @@ export default function Register() {
       <Header />
       <div className="mt-[60px] flex flex-col items-center bg-[#F6F5F4] w-full justify-evenly">
         <div className="w-[40%] bg-white my-[50px] flex flex-col p-[50px] rounded-md items-center">
+          {errorMessage ? (
+            <div className="bg-red-200 py-3 rounded-xl w-full flex items-center justify-center mb-4">
+              <span className="text-red-600">{"Error " + errorMessage}</span>
+            </div>
+          ) : (
+            <></>
+          )}
+
           <span className="font-primary text-[32px] mb-[30px]">Sign In</span>
 
           {/* Register with google button */}
@@ -63,6 +97,26 @@ export default function Register() {
 
           {/* input box */}
           <div className="w-[70%] items-start flex flex-col">
+            {/* First Name textbox */}
+            <span className="font-secondary text-[16px]">First Name :</span>
+            <input
+              type="text"
+              name="firstname"
+              value={firstName}
+              onChange={handleFirstNameChange}
+              className="w-full border-[1px] border-[#2B788B] rounded-lg mb-[10px] mt-[10px] px-[10px] pb-[3px] text-[16px] font-secondary"
+            />
+
+            {/* Username textbox */}
+            <span className="font-secondary text-[16px]">Last Name :</span>
+            <input
+              type="text"
+              name="lastname"
+              value={lastName}
+              onChange={handleLastNameChange}
+              className="w-full border-[1px] border-[#2B788B] rounded-lg mb-[10px] mt-[10px] px-[10px] pb-[3px] text-[16px] font-secondary"
+            />
+
             <span className="font-secondary text-[16px]">Email :</span>
             {/* Email textbox */}
             <input
@@ -72,15 +126,7 @@ export default function Register() {
               onChange={handleEmailChange}
               className="w-full border-[1px] border-[#2B788B] rounded-lg mb-[10px] mt-[10px] px-[10px] pb-[3px] text-[16px] font-secondary"
             />
-            <span className="font-secondary text-[16px]">Username :</span>
-            {/* Username textbox */}
-            <input
-              type="text"
-              name="username"
-              value={username}
-              onChange={handleUsernameChange}
-              className="w-full border-[1px] border-[#2B788B] rounded-lg mb-[10px] mt-[10px] px-[10px] pb-[3px] text-[16px] font-secondary"
-            />
+
             <span className="font-secondary text-[16px]">Password :</span>
             {/* Password textbox */}
             <input
@@ -104,23 +150,28 @@ export default function Register() {
           </div>
 
           {/* Role Selection */}
-          <div className="flex flex-row w-[50%] justify-around">
-            <div className="">
-              <span className="font-secondary text-[16px] mr-[12px] ${checkStudent}">
-                Student
-              </span>
-              <input
-                type="checkbox"
-              />
+          <div className="flex flex-row w-[70%] justify-around rounded-xl overflow-hidden mt-3 border border-black/[0.1]">
+            <button
+              className={`font-secondary flex flex-1 items-center justify-center text-[14px] py-3 ${
+                role === "student"
+                  ? "bg-blue-600 text-white"
+                  : "bg-white text-black"
+              }`}
+              onClick={setStudent}
+            >
+              I'm a Student
+            </button>
 
-              <span className="font-secondary text-[16px] mr-[12px] ml-[20px]">
-                Instructor
-              </span>
-              <input
-                type="checkbox"
-                onClick={() => setCheckInstructor((prev) => !prev)}
-              />
-            </div>
+            <button
+              className={`font-secondary flex flex-1 items-center justify-center text-[15px] py-3 ${
+                role === "instructor"
+                  ? "bg-blue-600 text-white"
+                  : "bg-white text-black"
+              }`}
+              onClick={setInstructor}
+            >
+              I'm an Instructor
+            </button>
           </div>
 
           {/* Register button */}
