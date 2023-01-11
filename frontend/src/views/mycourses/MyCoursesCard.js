fbfc689b394687941b1a99fcf38739d2f3b80b54
Zenith <sqoulo11@gmail.com>
2023-01-11 21:05:23 +0700

    edit: MyCourseCard style

--- frontend/src/views/mycourses/MyCoursesCard.js
+++ frontend/src/views/mycourses/MyCoursesCard.js
@@ -1,21 +1,27 @@
-export default function MyCourseCard() {
+export default function MyCourseCard({
+    courseName,
+    instructorName,
+    profile,
+    annoucement
+}) {
   return (
-    <li className="w-[400px] border-2 border-black my-[40px]">
+    <li className="w-[400px] border-[1px] border-black my-[40px] rounded-md">
       <div className="bg-white flex flex-col">
         <div className="pl-[20px] pb-[10px] w-full bg-[#2B788B] flex flex-row">
           <div className="mt-[40px] flex flex-col w-[70%]">
-            <span className="font-primary truncate ">
-              Course
-              eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+            <span className="font-primary truncate text-white text-[20px]">
+              {courseName}
             </span>
-            <span className="font-secondary truncate ">
-              Instructorrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+            <span className="font-secondary truncate text-white">
+                {instructorName}
             </span>
           </div>
-          <div className="mt-[10px] ml-[10px] h-[80px] w-[80px] rounded-full bg-slate-900"></div>
+          <div className="mt-[10px] ml-[10px] h-[80px] w-[80px] rounded-full overflow-hidden">
+            <img src={profile} alt="profile"></img>
+          </div>
         </div>
-        <div className="m-[20px] break-words line-clamp-4">
-            <span>Annoucementttttttttttttttttttttttttoucementttttttttttttttttttoucementtttttttttttttttttoucementttttttttttttttoucementttttttttttttttoucementttttttttttttttoucementttttttttttttttoucementttttttttttttttoucementttttttttttttttoucementttttttttttttttoucementtttttttttttttttoucementtttttttttttttttttttttt</span>
+        <div className="m-[20px] break-words line-clamp-4 h-[100px]">
+            <span>{annoucement}</span>
         </div>
         
       </div>
