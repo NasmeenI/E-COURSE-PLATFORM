aa4d248df9242c4cf1b7b5e0bcd3f86eb795d143
athensclub <43963760+athensclub@users.noreply.github.com>
2023-01-11 20:48:06 +0700

    edit: use arrow svg instead of plain text

--- frontend/src/views/details/Details.js
+++ frontend/src/views/details/Details.js
@@ -49,11 +49,22 @@ export default function Details() {
               Enroll now
             </span>
             <button
-              className="rounded-full bg-white border-2 border-[#639B6D] w-[20%] p-[5px]"
+              className="flex flex-row items-center justify-center rounded-full bg-white border-2 border-[#639B6D] w-[20%] p-[5px]"
               onClick={goBack}
             >
+              <svg
+                className="mr-3 overflow-hidden w-[1em] h-[1em]"
+                viewBox="0 0 1024 1024"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M810.666667 469.333333H304.64l154.88-186.026666a42.666667 42.666667 0 1 0-65.706667-54.613334l-213.333333 256a50.773333 50.773333 0 0 0-3.84 6.4c0 2.133333 0 3.413333-2.986667 5.546667A42.666667 42.666667 0 0 0 170.666667 512a42.666667 42.666667 0 0 0 2.986666 15.36c0 2.133333 0 3.413333 2.986667 5.546667a50.773333 50.773333 0 0 0 3.84 6.4l213.333333 256A42.666667 42.666667 0 0 0 426.666667 810.666667a42.666667 42.666667 0 0 0 27.306666-9.813334 42.666667 42.666667 0 0 0 5.546667-60.16L304.64 554.666667H810.666667a42.666667 42.666667 0 0 0 0-85.333334z"
+                  fill="#639B6D"
+                />
+              </svg>
               <span className="text-center text-[#639B6D] text-[16px] font-secondary font-semibold">
-                {"<- Return"}
+                {"Return"}
               </span>
             </button>
           </div>
