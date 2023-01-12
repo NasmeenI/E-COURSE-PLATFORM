1e5567c15b233ed5a8ff53ff4ac3c6ff27304943
athensclub <43963760+athensclub@users.noreply.github.com>
2023-01-12 17:30:34 +0700

    edit: show user profile image in header

--- frontend/src/components/header/ProfileData.js
+++ frontend/src/components/header/ProfileData.js
@@ -1,11 +1,23 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { auth } from "../../api/firebase";
 import { UserContext } from "../../contexts/UserContext";
+import FileAPI from "../../api/FileAPI";
 
 export default function ProfileData() {
+  const [imageUrl, setImageUrl] = useState("");
+
   const {
+    data: { user },
     action: { setUser },
   } = useContext(UserContext);
+
+  useEffect(() => {
+    async function fetchImage() {
+      setImageUrl(await FileAPI.getURL(user.image));
+    }
+
+    fetchImage();
+  }, [user]);
 
   function signOut() {
     auth.signOut();
@@ -14,11 +26,17 @@ export default function ProfileData() {
 
   return (
     <div className="flex flex-row items-center h-full">
-      <div className="rounded-full w-[50px] h-[50px] overflow-hidden bg-black"></div>
-      <span className="text-xl font-secondary">Passa</span>
+      <div className="rounded-full w-[44px] h-[44px] overflow-hidden bg-black">
+        <img src={imageUrl} alt="profile" />
+      </div>
+
+      <span className="text-lg font-secondary ml-[20px]">
+        {user.firstName + " " + user.lastName}
+      </span>
+
       <button
         onClick={signOut}
-        className="text-md font-secondary ml-[30px] font-extrabold border-[#2B788B] border-2 py-[3px] px-[15px] rounded-full"
+        className="text-md font-secondary ml-[20px] font-extrabold border-[#2B788B] border-2 py-[3px] px-[15px] rounded-full"
       >
         Sign Out
       </button>
